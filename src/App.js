877c59fa0d6fccb3f1fa1e8418eaaa7f431e17b3
mansourlotfi <mansourlotfi@gmail.com>
2020-02-03 17:34:08 +0330

    custome homeCard

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ import Home from './views/home.js';
 class App extends Component {
   render() {
     return (
-      
+      <div>
                <Route path="/" exact component={Home}/>
         
-      
+      </div>
     );
   }
 }
--- src/views/home.js
+++ src/views/home.js
@@ -1,19 +1,25 @@
 import React from "react";
 import Bg from '../assets/bg.jpg'
-
+import HomeCard from '../components/homeCard'
 function Home() {
 
   const bodyStyle = {
     backgroundImage: "url(" + Bg + ")",
-     width: '100%',
       height: '900px',
       // paddingTop : '0px',
       // marginTop: '0px'
   }
+  const cardStyle ={
+    width: '200px'
+  }
     return (
+      <>
       <div style={bodyStyle}>
         <h2 style={{color:'white', textAlign:'center'}}>Home asdasdasd</h2>
+        <HomeCard style={cardStyle}/>
       </div>
+
+      </>
     );
   }
 
