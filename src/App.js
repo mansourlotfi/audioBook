5675db9c3442da7f87519bd5169ed4036c10e13a
mansourlotfi <mansourlotfi@gmail.com>
2020-02-03 17:57:57 +0330

    clean up router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Link } from 'react-router-dom';
+import { Route, Link } from "react-router-dom";
 import './App.scss';
 import Home from './views/home.js';
 
