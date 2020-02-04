b130f433dec1518a5c3ae5f0a86d8236987903db
mansourlotfi <mansourlotfi@gmail.com>
2020-02-04 16:03:15 +0330

    structure change

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.scss';
 import Home from './views/home.js';
 import About from './views/about.js';
 import Dashboard from './views/dashboard.js';
-import EntryPage from './views/layouts/entryPage.js';
+import EntryPage from './views/entryPage/index.js';
 
 class App extends Component {
 	render() {
 		return (
-			<div>
+			<Router>
 				<Route path="/" exact component={Home} />
 				<Route path="/about" component={About} />
 				<Route path="/dashboard" component={Dashboard} />
 				<Route path="/entrypage" component={EntryPage} />
-			</div>
+			</Router>
 		);
 	}
 }
