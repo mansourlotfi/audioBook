226b02844fceb95ef3a31545edcab35d0686d2ab
mansourlotfi <mansourlotfi@gmail.com>
2020-02-06 19:23:18 +0330

    psd to jsx

--- src/views/entryPage/index.js
+++ src/views/entryPage/index.js
@@ -1,110 +1,34 @@
-import React from 'react';
-import './entrypage.scss';
-
-const { Component } = React;
+import React, { Component } from 'react';
+import './entryPage.css';
 
 export default class EntryPage extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			currentView: 'signUp'
-		};
-	}
-
-	changeView = (view) => {
-		this.setState({
-			currentView: view
-		});
-	};
-
-	currentView = () => {
-		switch (this.state.currentView) {
-			case 'signUp':
-				return (
-					<form>
-						<h2>Sign Up!</h2>
-						<fieldset>
-							<legend>Create Account</legend>
-							<ul>
-								<li>
-									<label for="username">Username:</label>
-									<input type="text" id="username" required />
-								</li>
-								<li>
-									<label for="email">Email:</label>
-									<input type="email" id="email" required />
-								</li>
-								<li>
-									<label for="password">Password:</label>
-									<input type="password" id="password" required />
-								</li>
-							</ul>
-						</fieldset>
-						<button>Submit</button>
-						<button type="button" onClick={() => this.changeView('logIn')}>
-							Have an Account?
-						</button>
-					</form>
-				);
-				break;
-			case 'logIn':
-				return (
-					<form>
-						<h2>Welcome Back!</h2>
-						<fieldset>
-							<legend>Log In</legend>
-							<ul>
-								<li>
-									<label for="username">Username:</label>
-									<input type="text" id="username" required />
-								</li>
-								<li>
-									<label for="password">Password:</label>
-									<input type="password" id="password" required />
-								</li>
-								<li>
-									<i />
-									<a onClick={() => this.changeView('PWReset')} href="#">
-										Forgot Password?
-									</a>
-								</li>
-							</ul>
-						</fieldset>
-						<button>Login</button>
-						<button type="button" onClick={() => this.changeView('signUp')}>
-							Create an Account
-						</button>
-					</form>
-				);
-				break;
-			case 'PWReset':
-				return (
-					<form>
-						<h2>Reset Password</h2>
-						<fieldset>
-							<legend>Password Reset</legend>
-							<ul>
-								<li>
-									<em>A reset link will be sent to your inbox!</em>
-								</li>
-								<li>
-									<label for="email">Email:</label>
-									<input type="email" id="email" required />
-								</li>
-							</ul>
-						</fieldset>
-						<button>Send Reset Link</button>
-						<button type="button" onClick={() => this.changeView('logIn')}>
-							Go Back
-						</button>
-					</form>
-				);
-			default:
-				break;
-		}
-	};
-
 	render() {
-		return <section id="entry-page">{this.currentView()}</section>;
+		return (
+			<div className="body">
+				<header>
+					<div className="container">
+						<nav>
+							<a href="/" className="brand">
+								IR-Programmers
+							</a>
+							<ul>
+								<li>خانه</li>
+								<li>مشاوره</li>
+								<li>امنیت</li>
+								<li>تیم ما</li>
+								<li>نمونه کارها</li>
+							</ul>
+						</nav>
+						<div className="header-row">
+							<div className="col-1">asd</div>
+							<div className="col-1">asd</div>
+							<div className="col-1">asd</div>
+							<div className="col-1">asd</div>
+						</div>
+					</div>
+				</header>
+				<body />
+			</div>
+		);
 	}
 }
