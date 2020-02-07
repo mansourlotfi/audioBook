2144b3c0a9c1ef8ae774d2a8f8d33a1c529fa23e
mansourlotfi <mansourlotfi@gmail.com>
2020-02-07 15:33:01 +0330

    basic login theme

--- src/views/entryPage/index.js
+++ src/views/entryPage/index.js
@@ -4,7 +4,7 @@ import './entryPage.css';
 export default class EntryPage extends Component {
 	render() {
 		return (
-			<div className="body">
+			<div className="loginBody">
 				<header>
 					<div className="container">
 						<nav>
@@ -17,6 +17,9 @@ export default class EntryPage extends Component {
 								<li>امنیت</li>
 								<li>تیم ما</li>
 								<li>نمونه کارها</li>
+								<a href="/home">
+									<li>ورود به پنل کاربری </li>
+								</a>
 							</ul>
 						</nav>
 						<div className="container2">
@@ -41,7 +44,34 @@ export default class EntryPage extends Component {
 						</div>
 					</div>
 				</header>
-				<body />
+				<div className="containerBody">
+					<div className="containerphoto">
+						<img src={require('../../assets/home/mobile.png')} alt="ir-programmers" />
+					</div>
+					<div className="containerText">
+						<span className="spanText">تست</span>
+					</div>
+				</div>
+				<div className="buttonContainer">
+					<ul>
+						<li>
+							<img src={require('../../assets/home/footer/col-4.png')} alt="آموزش" />
+							<h3>آموزش</h3>
+						</li>
+						<li>
+							<img src={require('../../assets/home/footer/col-3.png')} alt="امور حقوقی" />
+							<h3>امور حقوقی</h3>
+						</li>
+						<li>
+							<img src={require('../../assets/home/footer/col-2.png')} alt=" پشتیبانی" />
+							<h3> پشتیبانی</h3>
+						</li>
+						<li>
+							<img src={require('../../assets/home/footer/col-1.png')} alt="تماس با ما" />
+							<h3>تماس با ما</h3>
+						</li>
+					</ul>
+				</div>
 			</div>
 		);
 	}
