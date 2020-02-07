48b103f14d06334de792cc1b965e645f862a7d8e
mansourlotfi <mansourlotfi@gmail.com>
2020-02-07 13:20:58 +0330

    draft

--- src/views/entryPage/index.js
+++ src/views/entryPage/index.js
@@ -19,11 +19,25 @@ export default class EntryPage extends Component {
 								<li>نمونه کارها</li>
 							</ul>
 						</nav>
-						<div className="header-row">
-							<div className="col-1">asd</div>
-							<div className="col-1">asd</div>
-							<div className="col-1">asd</div>
-							<div className="col-1">asd</div>
+						<div className="container2">
+							<ul>
+								<li>
+									<img src={require('../../assets/home/header/col-4.png')} alt="مشاوره" />
+									<h3>مشاوره</h3>
+								</li>
+								<li>
+									<img src={require('../../assets/home/header/col-3.png')} alt="امنیت" />
+									<h3>امنیت</h3>
+								</li>
+								<li>
+									<img src={require('../../assets/home/header/col-2.png')} alt="تیم ما" />
+									<h3>تیم ما</h3>
+								</li>
+								<li>
+									<img src={require('../../assets/home/header/col-1.png')} alt="مشارکت" />
+									<h3>مشارکت</h3>
+								</li>
+							</ul>
 						</div>
 					</div>
 				</header>
