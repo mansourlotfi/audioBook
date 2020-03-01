e9789936ba742d2209fb4ea4589c88273f4538c1
mansourlotfi <mansourlotfi@gmail.com>
2020-03-01 20:41:17 +0330

    using axios to get data

--- src/views/entryPage/index.js
+++ src/views/entryPage/index.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react';
 import Header from './header';
 import Footer from './footer';
+import axios from 'axios';
 import './entryPage.css';
 
 export default class EntryPage extends Component {
+	state = {
+		persons: []
+	};
+
+	componentDidMount() {
+		axios.get(`https://jsonplaceholder.typicode.com/users`).then((res) => {
+			const persons = res.data;
+			this.setState({ persons });
+			console.warn(this.state.persons);
+		});
+	}
 	render() {
 		return (
 			<div className="loginBody">
 				<Header />
+				<ul style={{ color: 'white' }}>
+					{this.state.persons.map((person) => (
+						<li>
+							{person.name}
+							<br /> {person.email}
+						</li>
+					))}
+				</ul>
 
 				<Footer />
 			</div>
