260df986f1ea3bb2a0f95f877290871d489ab591
mansourlotfi <mansourlotfi@gmail.com>
2020-03-02 18:46:25 +0330

    use hooks

--- src/views/entryPage/index.js
+++ src/views/entryPage/index.js
@@ -1,36 +1,63 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './header';
 import Footer from './footer';
 import axios from 'axios';
+import Grid from '@material-ui/core/Grid';
+
+// import GridList from '@material-ui/core/GridList';
+// import GridListTile from '@material-ui/core/GridListTile';
+// import GridListTileBar from '@material-ui/core/GridListTileBar';
+// import { makeStyles } from '@material-ui/core/styles';
+
 import './entryPage.css';
 
-export default class EntryPage extends Component {
-	state = {
-		persons: []
-	};
+export default function EntryPage() {
+	const [ books, setBooks ] = useState([]);
+	// state = {
+	// 	books: []
+	// };
 
-	componentDidMount() {
-		axios.get(`https://jsonplaceholder.typicode.com/users`).then((res) => {
-			const persons = res.data;
-			this.setState({ persons });
-			console.warn(this.state.persons);
-		});
-	}
-	render() {
-		return (
-			<div className="loginBody">
-				<Header />
+	// componentDidMount() {
+	// 	axios.get(`https://picsum.photos/v2/list`).then((res) => {
+	// 		const books = res.data;
+	// 		this.setState({ books });
+	// 		console.warn(this.state.books);
+	// 	});
+	// }
+
+	useEffect(() => {
+		async function fetchData() {
+			setBooks(await axios.get(`https://picsum.photos/v2/list`).then((res) => res.data));
+		}
+		fetchData();
+	}, []);
+
+	return (
+		<div className="loginBody">
+			<Header />
+			{/* <div className="root">
+					<GridList className="gridList" cols={2.5}>
+						{this.state.books.map((book) => (
+							<GridListTile key={book.download_url}>
+								<img src={book.download_url} alt={book.url} />
+								<GridListTileBar title={book.author} />
+							</GridListTile>
+						))}
+					</GridList>
+				</div> */}
+			<div className="container2" style={{ padding: 100 }}>
 				<ul style={{ color: 'white' }}>
-					{this.state.persons.map((person) => (
+					{books.map((book) => (
 						<li>
-							{person.name}
-							<br /> {person.email}
+							<img src={book.download_url} alt="books" />
+							<br />
+							{book.author}
 						</li>
 					))}
 				</ul>
-
-				<Footer />
 			</div>
-		);
-	}
+
+			<Footer />
+		</div>
+	);
 }
